767c0cb45f1d19fed8f2e7ae2012924bd8b2a2b4
John Wikman <12082359+johnwikman@users.noreply.github.com>
2023-03-10 09:34:09 +0100

    Update miking hash and image version. Baseline unchanged (#11)

--- defs-common.mk
+++ defs-common.mk
@@ -5,7 +5,7 @@ MIKING_IMAGENAME=mikinglang/miking
 
 # NOTE: VERSION_SUFFIX to be set in subfolders' Makefile
 LATEST_VERSION=latest-$(VERSION_SUFFIX)
-MIKING_IMAGEVERSION=dev7-$(VERSION_SUFFIX)
+MIKING_IMAGEVERSION=dev8-$(VERSION_SUFFIX)
 BASELINE_IMAGEVERSION=v5-$(VERSION_SUFFIX)
 
 # The suffix for the version to be tagged with the `latest` alias
@@ -14,7 +14,7 @@ LATEST_ALIAS=latest-alpine
 BUILD_LOGDIR=../_logs
 
 MIKING_GIT_REMOTE="https://github.com/miking-lang/miking.git"
-MIKING_GIT_COMMIT="66b72cbe4678232858198a2b34c67222be46e627"
+MIKING_GIT_COMMIT="09c233080104f147a8fdd66fb63cfd6cfe8bf7b8"
 
 VALIDATE_IMAGE_SCRIPT=../scripts/validate_image.py
 VALIDATE_ARCH_SCRIPT="../scripts/validate_architecture.py"
