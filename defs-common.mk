e784ae2619533972a6cbd6593f304cff056f9343
John Wikman <12082359+johnwikman@users.noreply.github.com>
2023-05-20 02:57:24 +0200

    Update to latest commit and add JVM support (#13)
    * Update commit hash and add openjdk dependency
    
    * Update README with PR instructions

--- defs-common.mk
+++ defs-common.mk
@@ -5,8 +5,8 @@ MIKING_IMAGENAME=mikinglang/miking
 
 # NOTE: VERSION_SUFFIX to be set in subfolders' Makefile
 LATEST_VERSION=latest-$(VERSION_SUFFIX)
-MIKING_IMAGEVERSION=dev8-$(VERSION_SUFFIX)
-BASELINE_IMAGEVERSION=v5-$(VERSION_SUFFIX)
+MIKING_IMAGEVERSION=dev9-$(VERSION_SUFFIX)
+BASELINE_IMAGEVERSION=v6-$(VERSION_SUFFIX)
 
 # The suffix for the version to be tagged with the `latest` alias
 LATEST_ALIAS=latest-alpine
@@ -14,7 +14,7 @@ LATEST_ALIAS=latest-alpine
 BUILD_LOGDIR=../_logs
 
 MIKING_GIT_REMOTE="https://github.com/miking-lang/miking.git"
-MIKING_GIT_COMMIT="09c233080104f147a8fdd66fb63cfd6cfe8bf7b8"
+MIKING_GIT_COMMIT="73e2078e523f41bd51988016fe49841d5841716e"
 
 VALIDATE_IMAGE_SCRIPT=../scripts/validate_image.py
 VALIDATE_ARCH_SCRIPT="../scripts/validate_architecture.py"
