efcf4c1868a55532369a3b1787c4e301a6ac37db
John Wikman <john.wikman@hotmail.com>
2023-08-31 13:44:16 +0200

    Ping commit

--- defs-common.mk
+++ defs-common.mk
@@ -14,7 +14,7 @@ LATEST_ALIAS=latest-alpine
 BUILD_LOGDIR=../_logs
 
 MIKING_GIT_REMOTE="https://github.com/miking-lang/miking.git"
-MIKING_GIT_COMMIT="73e2078e523f41bd51988016fe49841d5841716e"
+MIKING_GIT_COMMIT="dfa6fac2561ee491ce4f4460f4ad4c6e0f34856a"
 
 VALIDATE_IMAGE_SCRIPT=../scripts/validate_image.py
 VALIDATE_ARCH_SCRIPT="../scripts/validate_architecture.py"
